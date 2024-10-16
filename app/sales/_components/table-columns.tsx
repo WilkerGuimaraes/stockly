777ad1b69271017fc8d902d94937d2f8bc4066e6
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-10-16 17:20:20 -0300

    feat: ensure sale can be deleted

--- app/sales/_components/table-columns.tsx
+++ app/sales/_components/table-columns.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { Button } from "@/app/_components/ui/button";
 import { SaleDto } from "@/app/_data-access/sale/get-sales";
 import { formatCurrency } from "@/app/_helpers/currency";
 import { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontalIcon } from "lucide-react";
+import SalesTableDropdownMenu from "./table-dropdown-menu";
 
 export const saleTableColums: ColumnDef<SaleDto>[] = [
   {
@@ -34,10 +33,8 @@ export const saleTableColums: ColumnDef<SaleDto>[] = [
   },
   {
     header: "Ações",
-    cell: () => (
-      <Button>
-        <MoreHorizontalIcon size={16} />
-      </Button>
+    cell: ({ row: { original: sale } }) => (
+      <SalesTableDropdownMenu sale={sale} />
     ),
   },
 ];
